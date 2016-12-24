b4771503effd6470892158dde1e522482c42a71f
Bill Glover <bill@billglover.co.uk>
2016-12-24 11:08:23 +0000

    Switched from environment variables to command line flags
    I’ve switched to using command line flags for the ease with which they
    can be implemented. I suspect that when running this API inside a
    Docker container, environment variables may be more useful. I propose
    writing a wrapper start-up script to set these where necessary.

--- main.go
+++ main.go
@@ -2,58 +2,56 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"log"
 	"net/http"
-	"os"
 	"sync"
 )
 
 // HealthResponse defines the structure of the response to a GET request on
 // the health API
 type HealthResponse struct {
-	Status      string  `json:"status"`
-	Delay       float32 `json:"delay"`
-	Jitter      float32 `json:"jitter"`
-	PayloadSize int32   `json:"payloadsize"`
-}
-
-var apiAddress string
-var healthAddress string
-var responseDelay float32
-var responseJitter float32
-var payloadSize int32
-
-func init() {
-	if apiAddress = os.Getenv("API_ADDRESS"); apiAddress == "" {
-		apiAddress = ":8080"
-	}
-	log.Println("configuration parameter: apiAddress", apiAddress)
-
-	if healthAddress = os.Getenv("HEALTH_ADDRESS"); healthAddress == "" {
-		healthAddress = ":8081"
-	}
-	log.Println("configuration parameter: healthAddress", healthAddress)
-
-	log.Println("API initialised")
+	Status          string `json:"status"`
+	Delay           int    `json:"delay"`
+	Jitter          int    `json:"jitter"`
+	PayloadSize     int    `json:"size"`
+	PayloadVariance int    `json:"variance"`
 }
 
 func main() {
 	log.Println("starting")
+
+	payloadSize := flag.Int("size", 1000, "the size of the payload in bytes")
+	payloadVar := flag.Int("variance", 0, "requested variance in payload size in bytes")
+	responseDelay := flag.Int("delay", 0, "the delay before returning API response")
+	responseJitter := flag.Int("jitter", 0, "requested variance in the delay before returning an API response")
+	apiAddress := flag.String("endpoint", ":8080", "requested variance in the delay before returning an API response")
+	healthAddress := flag.String("health", ":8081", "requested variance in the delay before returning an API response")
+
+	flag.Parse()
+
+	log.Printf("configuration parameter: size=%d\n", *payloadSize)
+	log.Printf("configuration parameter: variance=%d\n", *payloadVar)
+	log.Printf("configuration parameter: delay=%d\n", *responseDelay)
+	log.Printf("configuration parameter: jitter=%d\n", *responseJitter)
+	log.Printf("configuration parameter: endpoint=%s\n", *apiAddress)
+	log.Printf("configuration parameter: health=%s\n", *apiAddress)
 
 	var apiMux = http.NewServeMux()
 	var healthMux = http.NewServeMux()
 
 	apiMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain")
-		w.Write(RandStringBytesMaskImprSrc(1000))
+		w.Write(RandStringBytesMaskImprSrc(*payloadSize + *payloadVar))
 	})
 
 	healthMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		res := &HealthResponse{
-			Status:      "ok",
-			Delay:       responseDelay,
-			Jitter:      responseJitter,
-			PayloadSize: payloadSize,
+			Status:          "ok",
+			Delay:           *responseDelay,
+			Jitter:          *responseJitter,
+			PayloadSize:     *payloadSize,
+			PayloadVariance: *payloadVar,
 		}
 
 		resBytes, _ := json.Marshal(res)
@@ -66,8 +64,8 @@ func main() {
 	servers.Add(1)
 	go func() {
 		defer servers.Done()
-		log.Println("api starting on", apiAddress)
-		err := http.ListenAndServe(apiAddress, apiMux)
+		log.Println("api starting on", *apiAddress)
+		err := http.ListenAndServe(*apiAddress, apiMux)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -76,8 +74,8 @@ func main() {
 	servers.Add(1)
 	go func() {
 		defer servers.Done()
-		log.Println("health monitor starting on", healthAddress)
-		err := http.ListenAndServe(healthAddress, healthMux)
+		log.Println("health monitor starting on", *healthAddress)
+		err := http.ListenAndServe(*healthAddress, healthMux)
 		if err != nil {
 			log.Fatal(err)
 		}
